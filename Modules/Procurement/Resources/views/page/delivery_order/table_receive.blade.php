089544f42270e1d70ecd01c31f6b23d69396be0d
Itok toni <itok.toni@gmail.com>
2022-04-10 23:19:31 +0700

    update

--- Modules/Procurement/Resources/views/page/delivery_order/table_receive.blade.php
+++ Modules/Procurement/Resources/views/page/delivery_order/table_receive.blade.php
@@ -11,7 +11,7 @@
     <tbody class="markup">
         @foreach ($detail as $item)
         @php
-        $receive = Adapter::getTotalStockDoProduct($model->{$model->getKeyName()}, $item->mask_product_id);
+        $receive = Adapter::getTotalStockDoProduct($model->{$model->getKeyName()}, $item->mask_product_id, $item->mask_expired);
         $remaining = $item->mask_qty - $receive;
         @endphp
         <tr>
